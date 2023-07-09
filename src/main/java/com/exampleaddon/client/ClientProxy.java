a903f7a344c879364fee916410f5036082034a53
Sol-angelo <caydenward069@gmail.com>
2023-07-09 13:16:21 +1000

    added creative mode tabs

--- src/main/java/com/exampleaddon/client/ClientProxy.java
+++ src/main/java/com/exampleaddon/client/ClientProxy.java
@@ -13,6 +13,7 @@ import com.gempire.init.ModBlocks;
 import com.gempire.init.ModItems;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -22,7 +23,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+import static com.gempire.proxy.ClientProxy.GEMPIRE_GEMSTONES;
+
 public class ClientProxy {
+
+    public static CreativeModeTab ADDON_TAB;
 
     @Mod.EventBusSubscriber(modid = ExampleAddon.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModBusEvents {
@@ -46,23 +51,33 @@ public class ClientProxy {
             event.registerLayerDefinition(ModelTest.LAYER_LOCATION, ModelTest::createBodyLayer);
         }
 
+
+        //------THIS ADDS YOUR OWN CREATIVE MODE TAB-------
+
         @SubscribeEvent
-        public void buildContents(CreativeModeTabEvent.Register event) {
-            event.registerCreativeModeTab(new ResourceLocation(ExampleAddon.MODID, "gems"), builder ->
-                    // Set name of tab to display
-                    builder.title(Component.translatable("itemGroup.gems"))
+        public static void buildContents(CreativeModeTabEvent.Register event) {
+            ADDON_TAB = event.registerCreativeModeTab(new ResourceLocation(ExampleAddon.MODID, "items"),
+                    builder -> builder.title(Component.translatable("itemGroup.items"))
                             // Set icon of creative tab
                             .icon(() -> new ItemStack(AddonItems.WHITE_VARYTEST_GEM.get()))
-                            // Add default items to tab
                             .displayItems((params, populator) -> {
                                 populator.accept(AddonItems.WHITE_VARYTEST_GEM.get());
                                 populator.accept(AddonItems.PINK_VARYTEST_GEM.get());
-                                populator.accept(AddonItems.BLUE_VARYTEST_GEM.get());
                                 populator.accept(AddonItems.YELLOW_VARYTEST_GEM.get());
+                                populator.accept(AddonItems.BLUE_VARYTEST_GEM.get());
                                 populator.accept(AddonItems.TEST_GEM.get());
                             })
-            );
+                            .build());
         }
 
+        //-----THIS ADDS YOUR ITEMS TO GEMPIRES GEMSTONES TAB------
+
+        /*@SubscribeEvent
+        public void buildContents(CreativeModeTabEvent.BuildContents event) {
+            if (event.getTab() == GEMPIRE_GEMSTONES) {
+                event.accept(AddonItems.BLUE_VARYTEST_GEM.get());
+                event.accept(AddonItems.WHITE_VARYTEST_GEM.get());
+            }
+        }*/
     }
 }
